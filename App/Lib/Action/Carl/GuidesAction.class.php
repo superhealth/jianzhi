dca4a94986f4c1352e706fb6ec5a2c30a81ee837
Carl <609164964@qq.com>
2014-10-29 21:20:03 +0800

    guides

--- App/Lib/Action/Carl/GuidesAction.class.php
+++ App/Lib/Action/Carl/GuidesAction.class.php
@@ -1,37 +1,37 @@
 <?php
 /**
- * 栏目管理
+ * 使用指南
  * @author Carl
  * @date 2014-10-26
  */
-class ArticleAction extends BaseAction{
+class GuidesAction extends BaseAction{
 	/**
 	 * 列表
 	 */
 	public function index(){
-		$article = M("Pages");
+		$guides = M("Guides");
 		$map = array();
 		$param = array();
 		if(isset($_REQUEST['words'])){
 			$words = addslashes($_REQUEST['words']);
 			if(strlen($words)>=3){
-				$where['pg_name']  = array('like', "%{$words}%");
-				$where['pg_title']  = array('like', "%{$words}%");
+				$where['gu_name']  = array('like', "%{$words}%");
+				$where['gu_title']  = array('like', "%{$words}%");
 				$where['_logic'] = 'or';
 				$map['_complex'] = $where;
 				$param['words'] = $_REQUEST['words'];
 			}
 		}
 		$this->assign("param", $param);
-		$total = $article->where($map)->count();
+		$total = $guides->where($map)->count();
 		import("Org.Util.Page");
 		$page = new Page($total, 10, $param);
 		// 分页查询
 		$limit = $page->firstRow.",".$page->listRows;
 		$pager = $page->shown();
 		$this->assign("pager", $pager);
-		$order = "pg_order";
-		$lists = $article->order($order)->limit($limit)->select();
+		$order = "gu_order";
+		$lists = $guides->order($order)->limit($limit)->select();
 		//换色
 		if(!empty($param['words'])){
 			foreach($lists as &$v){
@@ -46,12 +46,12 @@ class ArticleAction extends BaseAction{
 	/**
 	 * 编辑
 	 */
-	public function editArticle(){
+	public function editGuides(){
 		$id = $_GET['id'];
 		if(empty($id)){
 			redirect(__URL__);
 		}else{
-			$info = M('Pages')->where('pg_id='.$id)->find();
+			$info = M('Guides')->where('gu_id='.$id)->find();
 			$this->assign('info', $info);
 			$this->display();
 		}
@@ -59,21 +59,21 @@ class ArticleAction extends BaseAction{
 	/**
 	 * 保存
 	 */
-	public function saveArticle(){
-		$art = M('Pages');
+	public function saveGuides(){
+		$art = M('Guides');
 		$data = $art->create();
-		if(isset($data['pg_id'])){
-			// 保存编辑
+		if(isset($data['gu_id'])){
+			// 保存修改
 			if($art->save($data)){
-				$this->watchdog("修改", '修改《'.$data['pg_title'].'》栏目');
-				$this->redirect(__URL__.'/editArticle/id/'.$data['pg_id']);
+				$this->watchdog("修改", '修改《'.$data['gu_title'].'》栏目');
+				$this->redirect(__URL__.'/editArticle/id/'.$data['gu_id']);
 			}else{
 				$this->error('保存失败！');
 			}
 		}else{
 			// 保存新增
 			if($art->add($data)){
-				$this->watchdog("新增", '新增加栏目《'.$data['pg_title'].'》');
+				$this->watchdog("新增", '新增加栏目《'.$data['gu_title'].'》');
 				$this->redirect(__URL__);
 			}else{
 				$this->error('保存失败！');
@@ -83,19 +83,19 @@ class ArticleAction extends BaseAction{
 	/**
 	 * 添加
 	 */
-	public function addArticle(){
+	public function addGuides(){
 		$this->display();
 	}
 	/**
 	 * 删除
-	 */
-	public function delArticle(){
+	 */	
+	public function delGuides(){
 		$id = $_REQUEST['id'];
 		$map = array(
-			'pg_id'	=> array('in', $id)
+			'gu_id'	=> array('in', $id)
 		);
-		$names = M('Pages')->where($map)->getField('pg_title', true);
-		if(M('Pages')->where($map)->delete()){
+		$names = M('Guides')->where($map)->getField('gu_title', true);
+		if(M('Guides')->where($map)->delete()){
 			$this->watchdog('删除', '删除栏目《'.implode('》《', $names).'》');
 			$this->redirect(__URL__);
 		}else{
